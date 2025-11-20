afcc931c77de0324146c74b634fbc7446423faa2
root <root@DESKTOP-51DFP8C.localdomain>
2025-11-20 12:28:46 +0000

    .ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { resolve } from 'path'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/ZedCTF/' : '/',
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    emptyOutDir: true,
-  },
-  server: {
-    port: 3000,
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
+  base: '/ZedCTF/',   // <-- IMPORTANT
 })
