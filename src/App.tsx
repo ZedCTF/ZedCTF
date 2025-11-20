f8402fbe8461058f1ed7c76803150419c50baa11
James Soko <s0k0j4m3s@gmail.com>
2025-11-20 09:44:35 +0000

    Revamp App component with new layout and content

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import Home from './pages/Home';
+import React from 'react';
 
 function App() {
-  return <Home />;
+  return (
+    <div style={{ padding: '20px', backgroundColor: 'green', color: 'white' }}>
+      <h1>ZedCTF - Zambia Cybersecurity</h1>
+      <p>If you can see this, React is working!</p>
+      <button onClick={() => alert('Hello Zambia!')}>Click me</button>
+    </div>
+  );
 }
 
 export default App;
