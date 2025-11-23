d55dc5e696c3bcb16824800a156d8f025d10f400
root <root@DESKTOP-51DFP8C.localdomain>
2025-11-23 09:33:55 +0000

    Registration Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Index from './pages/Index';
 import Login from './pages/Login';
+import Signup from './pages/Signup'; // Add this import
 import NotFound from './pages/NotFound';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} /> {/* Add this route */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
